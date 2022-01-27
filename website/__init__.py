7ac1342e6165f773fbcbe2f911a53ffc17adfea5
Chronicsanity <71952344+Chronicsanity@users.noreply.github.com>
2022-01-26 21:35:20 -0600

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -11,20 +11,20 @@ db = SQLAlchemy()
 def create_app():
     
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rpojgsgfhigprq:3e74d2ed51b8ad75dadd84b7404ac6761f19396439f75c48c4921cf97e4b2b88@ec2-52-70-107-254.compute-1.amazonaws.com:5432/d1aldo6rvck7l1'
     app.config['SECRET_KEY'] = 'mr. worldwide'
     ENV ='prod'
     
     if ENV == 'dev':
         app.debug = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/sra'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rpojgsgfhigprq:3e74d2ed51b8ad75dadd84b7404ac6761f19396439f75c48c4921cf97e4b2b88@localhost/d1aldo6rvck7l1'
         
     else:
         app.debug = False
-        app.config['SQLACHEMY_DATABASE_URI'] = 'postgres://mhgqtisxrnhajo:47830ca44a024943e000deda5eb86a37b84fb1e4dd79bec1cc233702ce32a99e@ec2-3-225-132-26.compute-1.amazonaws.com:5432/d3m22ojr7fksk2'
+        app.config['SQLACHEMY_DATABASE_URI'] = 'postgres://rpojgsgfhigprq:3e74d2ed51b8ad75dadd84b7404ac6761f19396439f75c48c4921cf97e4b2b88@ec2-52-70-107-254.compute-1.amazonaws.com:5432/d1aldo6rvck7l1'
 
     
     db.init_app(app)
-
 
     from .views import views
     from .auth import auth
@@ -33,7 +33,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User
-
+    
     create_database(app)
 
     login_manager = LoginManager()
